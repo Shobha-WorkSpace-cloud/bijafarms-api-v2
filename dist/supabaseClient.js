e563703f1cf4a7cb9cfc7a93a9f7d789c0c6f264
Shobha-WorkSpace-cloud <shobha.sangi@outlook.com>
2025-08-14 12:37:28 +0530

    second checckin

--- dist/supabaseClient.js
+++ dist/supabaseClient.js
@@ -7,7 +7,7 @@ var supabase_js_1 = require("@supabase/supabase-js");
 var dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 var supabaseUrl = 'https://dbmthxrbrlgkuhiznsul.supabase.co';
-var supabaseKey = process.env.SUPABASE_KEY;
+var supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRibXRoeHJicmxna3VoaXpuc3VsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NTU0ODEsImV4cCI6MjA3MDEzMTQ4MX0.b6gFaZcT5AdVPomr7U-5Y2S_slIqza_4zeCtkC5s8Kc';
 if (!supabaseKey) {
     throw new Error('SUPABASE_KEY is not defined in environment variables');
 }
